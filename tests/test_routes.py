6a2128d8c72d2ddc2eb00e2e0ff35d14fdab93f4
ulysse104 <ulysselary@gmail.com>
2025-10-26 18:52:50 -0400

    Update Account: Test Case and Code to Make the Test Case Pass

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -145,4 +145,18 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         data = response.get_json()
         self.assertIn("Account not found", data["message"])
-  
+
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        resp = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(resp.status_code, status.HTTP_201_CREATED)
+
+        # update the account
+        new_account = resp.get_json()
+        new_account["name"] = "Something Known"
+        resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        updated_account = resp.get_json()
+        self.assertEqual(updated_account["name"], "Something Known")
