12bddc83943106295af5c3f0877f3f8792b2971c
ulysse104 <ulysselary@gmail.com>
2025-10-27 19:31:07 -0400

    Correction Lint

--- service/routes.py
+++ service/routes.py
@@ -75,8 +75,7 @@ def list_accounts():
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
-
-
+    
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
@@ -98,6 +97,7 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
+
 
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
